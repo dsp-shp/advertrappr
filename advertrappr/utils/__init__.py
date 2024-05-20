a1cec3cbe35390e2719cd72f826f3767b5f8dc86
Ivan <dsp_shp@icloud.com>
2024-05-20 15:21:57 +0300

    fix(main & utils): fix main.py run CLI args parsing & utils imports

--- advertrappr/utils/__init__.py
+++ advertrappr/utils/__init__.py
@@ -1,5 +1,5 @@
 from .service import Advert, getService
 from .connector import connect
-from .logger import getLogger, updateLoggers
+from .logger import getLogger
 from .messenger import send_messages
 
